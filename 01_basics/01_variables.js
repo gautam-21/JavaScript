c427517c1f3e9a615becf4f6eee93654817df8c5
Gautam <gautam.kumarsit2019@gmail.com>
2024-10-14 08:59:16 +0530

    let and var diff is added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -15,3 +15,23 @@ console.log(id);
 
 // if we not assign the value then it assign the undefined as value
 console.table([name, id, subject, marks, roolno]);
+console.log(id);
+id=1;
+console.log('new id', id); //show error
+
+
+
+// const - variables canoot be changed
+/* var vs let
+1. Scope
+var: Function-scoped or globally-scoped (if not inside a function).
+let: Block-scoped (limited to the nearest {} block, such as loops, if statements, or functions).
+
+2. Hoisting
+var: Is hoisted to the top of its scope, but initialized with undefined (accessible before its declaration, though not recommended).
+let: Is also hoisted but not initialized. Accessing it before declaration leads to a ReferenceError (called the "temporal dead zone").
+
+3. Re-declaration
+var: Allows re-declaration of the same variable in the same scope.
+let: Does not allow re-declaration in the same scope.
+*/
