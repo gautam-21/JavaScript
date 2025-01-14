7519e117d1abbcbbbfc156966726d527d0537c1c
gautam-21 <82769185+gautam-21@users.noreply.github.com>
2025-01-14 18:19:53 +0000

    object part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,47 @@
+// two way to make object - 1. constructor (singleton - single instance is there)
+// 2. literals - multiple instance 
+
+// single ton e.g 
+const obj = Object.create
+
+//literals
+const ob = {}
+
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
