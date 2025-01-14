3b4a0da0b03edef7b185ad1702af2711285a55e9
gautam-21 <82769185+gautam-21@users.noreply.github.com>
2025-01-14 19:07:33 +0000

    added object de structure

---
+++ 02_basics/test.js
@@ -0,0 +1,12 @@
+// object de structure
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
