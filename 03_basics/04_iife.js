52b255795b0ab25cb3c805965bdde6944bab3ac6
gautam-21 <82769185+gautam-21@users.noreply.github.com>
2025-01-25 20:56:04 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -4,7 +4,7 @@
 
 // ()()  //here 1st () is where we write the function definition and 2nd one is used for execution
 
-
+// globalscope ke pollution se problem hti hai esliye aisa likhte hai
 
 (function chai(){
     // named IIFE
