9448a81b19f3511ed574c8ed4918eac560ec667f
gautam-21 <82769185+gautam-21@users.noreply.github.com>
2025-03-30 18:11:54 +0000

    for of, for in, for each, map

---
+++ 05_iterations/four.js
@@ -0,0 +1,34 @@
+// for in - used to iterate the object
+
+const object = { a: 1, b: 2, c: 3 };
+
+for (const key in object) {
+  console.log(`${key}: ${object[key]}`);
+}
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
